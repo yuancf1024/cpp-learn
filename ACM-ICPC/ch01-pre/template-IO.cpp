b46f87d04bee8f4bc8ceb94d4ecce9ebb57988e1
“yuancf1024” <3455911441@qq.com>
2022-05-10 22:31:22 +0800

    IO 模板

--- ACM-ICPC/ch01-pre/template-IO.cpp
+++ ACM-ICPC/ch01-pre/template-IO.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 const int MAX_N = 50;
+int n, m, k[MAX_N];
 
 // 核心代码，自定义函数
 void solve() {
@@ -16,8 +19,9 @@ int main() {
     }
 
     // 输出到标准输出
-    if (solve) puts("Yes");
-    else puts("No");
+    // if (solve) puts("Yes");
+    // else puts("No");
+    solve();
 
     return 0;
 }
