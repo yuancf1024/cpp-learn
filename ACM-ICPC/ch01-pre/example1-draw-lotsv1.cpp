7f27fa44b5f7e57fa3a089c0c7652cb5d1024b7f
“yuancf1024” <3455911441@qq.com>
2022-05-13 21:00:18 +0800

    挑战程序设计竞赛ch01

--- ACM-ICPC/ch01-pre/example1-draw-lotsv1.cpp
+++ ACM-ICPC/ch01-pre/example1-draw-lotsv1.cpp
@@ -46,13 +46,21 @@ void solve1() {
 
 
 /*############ 二分搜索与O(n^3logn)的算法 ############*/
+
+/*
+排序O(nlogn)时间
+循环O(n^3logn)时间
+总时间：O(n^3logn)
+*/
+
+// 二分搜索
 bool binary_search(int x) {
     // x的存在范围是k[l], k[l+1],...,k[r-1]
     int l = 0, r = n;
 
     // 反复操作直到存在范围为空
     while (l <= r) { // r - l >= 1
-        int i = l + ((r - l) >> 2);
+        int i = l + ((r - l) >> 1);
         if (k[i] == x) return true; // 找到x
         else if (k[i] < x) l = i + 1;
         else r = i - 1;
@@ -78,6 +86,65 @@ void solve2() {
         }
     }
     // 输出到标准输出
+    if (f) puts("Yes");
+    else puts("No");
+}
+
+/*############ O(n^2logn)的算法 ############*/
+
+/*
+对内侧的两个循环进一步优化:
+预先枚举出kc+kd所得的n^2个数字并排好序，然后利用二分搜索
+
+排序O(n^2logn)时间
+循环O(n^2logn)时间
+总时间：O(n^2logn)
+*/
+
+// 输入
+// const int MAX_N = 50;
+// int n, m, k[MAX_N];
+
+// 保存2个数的和的数列
+int kk[MAX_N * MAX_N];
+
+// 二维数组的二分搜索
+bool binary_search1(int x) {
+    // x的存在范围是kk[l], kk[l+1], ..., kk[r-1]
+    int l = 0, r = n * n;
+
+    // 反复操作直到存在范围为空
+    while (l <= r) {
+        int i = l + ((r-l)>>1);
+        if (kk[i] == x) return true; // 找到x
+        else if (kk[i] < x) l = i + 1;
+        else r = i - 1;
+    }
+    // 没找到x
+    return false;
+}
+
+void solve3() {
+    // 枚举k[c]+k[d]的和
+    for (int c = 0; c < n; c++) {
+        for (int d = 0; d < n; d++) {
+            kk[c*n+d] = k[c] + k[d];
+        }
+    }
+
+    // 排序以便进行二分搜索
+    sort(kk, kk + n*n);
+
+    // bool f = false;
+    for (int a = 0; a < n; a++) {
+        for (int b = 0; b < n; b++) {
+            // 将内侧的两个循环替换成二分搜索
+            if (binary_search1(m - k[a] - k[b])) {
+                f = true;
+            }
+        }
+    }
+
     if (f) puts("Yes");
     else puts("No");
 }
