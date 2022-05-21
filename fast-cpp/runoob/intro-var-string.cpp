00960b1a562c91ac42fa0e135a427ba94b6272fc
“yuancf1024” <3455911441@qq.com>
2022-05-21 22:20:33 +0800

    C++菜鸟教程快速入门2

---
+++ fast-cpp/runoob/intro-var-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string greeting = "hello, runoob";
+    cout << greeting;
+    cout << "\n";     // 换行符
+    string greeting2 = "hello, \
+runoob";
+    cout << greeting2;
+    return 0;
+}
