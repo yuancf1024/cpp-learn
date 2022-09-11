63e84f68f886cccd7998016fd34ab23b93a121f9
yuancf1024 <3455911441@qq.com>
2022-09-11 18:18:56 +0800

    C++常用输入输出读取函数

---
+++ Test/16-ACM-IO.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() { 
+    // 读取单个数字
+    // int n, m;
+    // cin >> n >> m;
+
+    // // 读取字符串
+    // string s;
+    // cin >> s;
+
+    // 读取一行字符串
+    string str1,str2;
+    getline(cin, str1);
+    // str2 = cin.getline();
+    // 读取单个字符
+    char t;
+    t = cin.get();
+
+    // cout << "n=" << n << endl;
+    // cout << "m=" << m << endl;
+    // cout << "s=" << s << endl;
+    cout << "str1=" << str1 << endl;
+    cout << "t=" << t << endl;
+
+    return 0;
+}
+
+/*
+
+
+// 单独运行
+chris
+a
+
+str1=chris
+t=a
+*/
